8d055abccd100868eff4484638aa218b54035bfa
Szikszai László <laszlo_szikszai@epam.com>
2024-02-12 09:56:19 +0100

    Update git.sh

--- steps/git.sh
+++ steps/git.sh
@@ -6,3 +6,10 @@ git config --global user.email "$EMAIL"
 
 read -p "Git username:" USERNAME
 git config --global user.name "$USERNAME"
+
+# Handy commands and settings
+# Source: https://www.youtube.com/watch?v=aolI_Rz0ZqY
+git config --global rerere.enabled true
+git config --global alias.blaame "blame -w -C -C -C"
+git config --global column.ui auto
+git config --global branch.sort -committerdate
